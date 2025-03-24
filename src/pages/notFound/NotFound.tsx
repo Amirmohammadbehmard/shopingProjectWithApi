19d925e425f5a54b57fbb72d2c1ec0e83c2890a4
amirmb <batbeginss@gmail.com>
2025-03-24 21:35:28 +0330

    third bug fix

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -6,14 +6,16 @@ function NotFound() {
       <div className="text-center space-y-6">
         <h1 className="text-9xl font-bold text-blue-500">404</h1>
 
-        <h2 className="text-3xl font-semibold dark:text-white text-black">Oops! Page Not Found</h2>
+        <h2 className="text-3xl font-semibold dark:text-white text-black">
+          Oops! Page Not Found
+        </h2>
         <p className="text-lg dark:text-white text-black">
           The page you are looking for does not exist or has been moved.
         </p>
 
         <div className="flex flex-col sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4">
           <Link
-            to="/"
+            to="/shopingProjectWithApi/"
             className="w-full sm:w-auto px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
           >
             Go to Home
